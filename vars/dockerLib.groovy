8f425183c2ffc75a2ada607ada6c35bef7036ea1
demoperiferia <101884714+DemosPeriferiaDevOps@users.noreply.github.com>
2023-02-08 08:56:13 -0500

    Update dockerLib.groovy

--- vars/dockerLib.groovy
+++ vars/dockerLib.groovy
@@ -6,6 +6,10 @@ def push(Map params){
     sh "sudo docker push ${params.DockerImage}"
 }
 
+def remove(Map params){
+    sh "sudo docker rmi ${params.DockerImage}"
+}
+
 def promoter(Map params){
     sh "sudo docker pull ${params.DockerImage}"
     sh "sudo docker tag ${params.DockerImage} ${params.DockerNewImage}"
